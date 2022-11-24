320d8c5c6b652232485f554cb449f8a89631bcdb
phonzammi <phonzammizaki@gmail.com>
2022-11-24 15:04:42 +0700

    Judul di halaman login

--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -2,6 +2,7 @@
     <div class="row justify-content-center my-5">
         <div class="col-sm-12 col-md-8 col-lg-5 my-4">
             <div>
+                <h3 class="text-center text-uppercase font-weight-bold font-italic">Sistem Manajemen Dokumen</h3>
                 {{ $logo }}
             </div>
 
